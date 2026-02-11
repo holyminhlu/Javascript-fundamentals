c864176c22b31fac964d96daa88256d6c0a978c8
minhlu <holyminhlu@gmail.com>
2026-02-11 20:51:07 +0700

    Data Type-2

---
+++ src/video19/video19.js
@@ -0,0 +1,16 @@
+console.log("Video 19")
+
+//objects: key - value
+
+const person = {
+    name: "Minh Lu",
+    age: 22,
+    address: "Vinh Long"
+}
+
+console.log(person)
+
+//array
+
+const scores = [10, 3,4,8,9]
+console.log(scores)
