0506d38f672a9c876769d61cc977f30ca7522255
minhlu <holyminhlu@gmail.com>
2026-02-11 21:45:21 +0700

    Template String

---
+++ src/video22/video22.js
@@ -0,0 +1,12 @@
+console.log("Video 22");
+
+//template Strings
+const myName = "hoidanit";
+const age = 28;
+
+const introduction = "my name is " + myName + " \n years old:"+ age;
+console.log(introduction)
+
+const intro = `my name is "${myName}" 
+years old: ${age}`;
+console.log(intro)
