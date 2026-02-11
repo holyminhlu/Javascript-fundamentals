6d4db8dc82c6a22ab4005d593ad14c29a17158ba
minhlu <holyminhlu@gmail.com>
2026-02-11 21:31:30 +0700

    Cac loai Console

---
+++ src/video21/video21.js
@@ -0,0 +1,30 @@
+console.log("Video 21");
+
+// ABC
+/* 
+    ABC 
+*/
+
+const name = "Video 21";
+const age = 21;
+
+//lenh console
+console.log("Thong bao thong thuong");
+console.error("Thong bao loi");
+console.warn("Thong bao canh bao");
+
+//Tip
+let name1 = "Nguyen Van A"; //string
+let age1 = 30; //number
+
+console.log(name1, age1);
+
+//khong nen cong chuoi de sai
+console.log("Toi la: " + name1 + ", nam nay toi " + age1 + " tuoi.");
+//nen su dung template string
+console.log(`Toi la: ${name1}, nam nay toi ${age1} tuoi.`);
+//Hoac
+console.log("Toi la:", name1, ", nam nay toi", age1, "tuoi.");
+
+// Them mau cho console
+console.log("%c Thong bao co mau", "color: red; font-weight: bold;");
