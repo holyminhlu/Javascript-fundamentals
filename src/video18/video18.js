d3437d56deb737398b748f5fabd857a5474238dc
minhlu <holyminhlu@gmail.com>
2026-02-11 20:45:33 +0700

    Data Type

---
+++ src/video18/video18.js
@@ -0,0 +1,31 @@
+// number
+ const myAge = 25;
+ const myScore = 9.1;
+
+ //string
+
+ const name = "Hoidanit";  //Java
+ const name1 ='Hoidanit';
+ const name2 = `Hoidanit`;
+
+//boolean: true/false
+
+const isTuiDepTrai = true;
+
+//Undefined
+
+let bla;
+console.log(bla);
+
+//Null
+
+let testNull = null;
+console.log(testNull);
+
+console.log("Video 18");
+console.log(myAge);
+console.log(myScore);
+console.log(name);
+console.log(isTuiDepTrai);
+
+console.log(typeof myAge);
