45fe2112bc8e7b9af4b3ef8d7b6ebede6ce314d3
minhlu <holyminhlu@gmail.com>
2026-02-12 22:09:12 +0700

    Function

---
+++ src/Chapter04/video30/video30.js
@@ -0,0 +1,16 @@
+console.log("Video 30");
+
+function greeting(){
+    console.log("Hello from Video 30");
+}
+
+greeting();
+
+// sum a+b
+
+function sum(a, b){   // parameters
+    return a + b;
+}
+console.log(sum(15, 10));
+console.log(sum("Anh", 10));
+console.log(sum("Anh", "Mini"));
