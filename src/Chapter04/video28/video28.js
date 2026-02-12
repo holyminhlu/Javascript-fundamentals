31d2abeab3784cbf4039f91c71d46a1acb34aa34
minhlu <holyminhlu@gmail.com>
2026-02-12 21:50:09 +0700

    Loop 3

---
+++ src/Chapter04/video28/video28.js
@@ -0,0 +1,23 @@
+console.log("Video 28");
+
+
+//for
+for(let i = 0; i <= 5; i++) {
+    console.log(`Gia tri i = ${i}`);
+}
+
+//while
+
+let scores = 10;
+ while(scores > 5) {
+    console.log(`>>> Diem cua ban la ${scores}`);
+    scores--;
+ }
+
+ //do...while  -> Chay it nhat 1 lan
+
+ let scores1 = 10;
+ do {
+    console.log(`>>> Diem cua ban la ${scores1}`);
+    scores1--;
+ } while (scores1 > 5);
