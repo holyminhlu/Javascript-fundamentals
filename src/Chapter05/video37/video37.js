5b272d86efbb1bd7f981f2c40dd251a981f3984a
minhlu <holyminhlu@gmail.com>
2026-02-22 16:02:58 +0700

    Array

---
+++ src/Chapter05/video37/video37.js
@@ -0,0 +1,10 @@
+console.log("Video 37");
+
+const names = ["Alice", "Bob", "Charlie", "David", "Eve"];
+
+const names2 = ["Alice", "Bob", [["Charlie", "David"], "Eve"]];
+
+
+
+console.log(names);
+console.log(names2);
