ee5332967550784db3c9cf8db98ddd64242ef92c
minhlu <holyminhlu@gmail.com>
2026-02-22 16:48:28 +0700

    Array Object

---
+++ src/Chapter05/video42/video42.js
@@ -0,0 +1,24 @@
+console.log("Video 42");
+
+const scores = [10, 20, 30, 40, 50];
+
+const sv1 = {
+    score:10,
+    name:"John",
+    adress:{
+        city:"New York",
+        country:"USA"
+    }
+}
+const sv2 = {
+    score:20,
+    name:"Jane",
+    adress:{
+        city:"New York",
+        country:"USA"
+    }
+}
+
+const sinhvien =[sv1,sv2];
+
+console.log(sinhvien);
